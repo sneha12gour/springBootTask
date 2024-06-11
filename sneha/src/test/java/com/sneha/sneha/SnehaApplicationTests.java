da31eee6b1f2d0bfbacbe56a6d9f7922cf11f529
Sneha G <snehagour77@gmail.com>
2024-06-11 10:38:47 +0530

    initial commit

---
+++ sneha/src/test/java/com/sneha/sneha/SnehaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.sneha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnehaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
